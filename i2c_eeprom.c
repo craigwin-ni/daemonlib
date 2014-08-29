56d103e961fe3f471f7ede3526fa6436f59d728e
Matthias Bolte <matthias@tinkerforge.com>
2014-08-29 15:42:12 +0200

    i2c-eeprom: Add missing copyright and license headers

--- i2c_eeprom.c
+++ i2c_eeprom.c
@@ -1,3 +1,25 @@
+/*
+ * daemonlib
+ * Copyright (C) 2014 Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
+ * Copyright (C) 2014 Matthias Bolte <matthias@tinkerforge.com>
+ *
+ * i2c_eeprom.c: I2C EEPROM specific functions
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <errno.h>
 #include <stdint.h>
 #include <sys/types.h>
