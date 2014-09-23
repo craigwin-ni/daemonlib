a8cec26660f2f4a3d58f53919834030cb359f594
Matthias Bolte <matthias@tinkerforge.com>
2014-09-23 17:09:24 +0200

    Move event source array iteration into function

--- event.c
+++ event.c
@@ -88,6 +88,27 @@ void event_exit(void) {
 	array_destroy(&_event_sources, NULL);
 }
 
+static EventSource *event_find_source(int start, int end, IOHandle handle,
+                                      EventSourceType type, int *index) {
+	int i;
+	int step = start < end ? 1 : -1;
+	EventSource *event_source;
+
+	for (i = start; i != end; i += step) {
+		event_source = array_get(&_event_sources, i);
+
+		if (event_source->handle == handle && event_source->type == type) {
+			if (index != NULL) {
+				*index = i;
+			}
+
+			return event_source;
+		}
+	}
+
+	return NULL;
+}
+
 // the event sources array contains tuples (handle, type). each tuple can be
 // in the array only once. trying to add (5, USB) to the array while such a
 // tuple is already in the array is an error. there is one exception from this
@@ -96,242 +117,235 @@ void event_exit(void) {
 // got marked as removed before
 int event_add_source(IOHandle handle, EventSourceType type, uint32_t events,
                      EventFunction function, void *opaque) {
-	int i;
+	int index;
 	EventSource *event_source;
 	EventSource backup;
 
-	// check existing event sources
-	for (i = 0; i < _event_sources.count; ++i) {
-		event_source = array_get(&_event_sources, i);
+	event_source = event_find_source(0, _event_sources.count, handle, type, &index);
 
-		if (event_source->handle == handle && event_source->type == type) {
-			if (event_source->state == EVENT_SOURCE_STATE_REMOVED) {
-				memcpy(&backup, event_source, sizeof(backup));
-
-				event_source->events = events;
-				event_source->state = EVENT_SOURCE_STATE_READDED;
-
-				if ((events & EVENT_READ) != 0) {
-					event_source->read = function;
-					event_source->read_opaque = opaque;
-				}
-
-				if ((events & EVENT_WRITE) != 0) {
-					event_source->write = function;
-					event_source->write_opaque = opaque;
-				}
-
-				if ((events & EVENT_PRIO) != 0) {
-					event_source->prio = function;
-					event_source->prio_opaque = opaque;
-				}
-
-				if ((events & EVENT_ERROR) != 0) {
-					event_source->error = function;
-					event_source->error_opaque = opaque;
-				}
-
-				if (event_source_added_platform(event_source) < 0) {
-					memcpy(event_source, &backup, sizeof(backup));
-
-					return -1;
-				}
-
-				log_debug("Readded %s event source (handle: %d) at index %d",
-				          event_get_source_type_name(type, false), handle, i);
-
-				return 0;
-			}
-
-			log_error("%s event source (handle: %d) already added at index %d",
-			          event_get_source_type_name(type, true), handle, i);
-
-			return -1;
-		}
-	}
-
-	// add event source
-	event_source = array_append(&_event_sources);
-
-	if (event_source == NULL) {
-		log_error("Could not append to event source array: %s (%d)",
-		          get_errno_name(errno), errno);
-
-		return -1;
-	}
-
-	event_source->handle = handle;
-	event_source->type = type;
-	event_source->events = events;
-	event_source->state = EVENT_SOURCE_STATE_ADDED;
-
-	if ((events & EVENT_READ) != 0) {
-		event_source->read = function;
-		event_source->read_opaque = opaque;
-	}
-
-	if ((events & EVENT_WRITE) != 0) {
-		event_source->write = function;
-		event_source->write_opaque = opaque;
-	}
-
-	if ((events & EVENT_PRIO) != 0) {
-		event_source->prio = function;
-		event_source->prio_opaque = opaque;
-	}
-
-	if ((events & EVENT_ERROR) != 0) {
-		event_source->error = function;
-		event_source->error_opaque = opaque;
-	}
-
-	if (event_source_added_platform(event_source) < 0) {
-		array_remove(&_event_sources, _event_sources.count - 1, NULL);
-
-		return -1;
-	}
-
-	log_debug("Added %s event source (handle: %d, events: %d) at index %d",
-	          event_get_source_type_name(type, false),
-	          handle, events, _event_sources.count - 1);
-
-	return 0;
-}
-
-// the events that an event source was added for can be modified
-int event_modify_source(IOHandle handle, EventSourceType type, uint32_t events_to_remove,
-                        uint32_t events_to_add, EventFunction function, void *opaque) {
-	int i;
-	EventSource *event_source;
-	EventSource backup;
-
-	for (i = 0; i < _event_sources.count; ++i) {
-		event_source = array_get(&_event_sources, i);
-
-		if (event_source->handle == handle && event_source->type == type) {
-			if (event_source->state == EVENT_SOURCE_STATE_REMOVED) {
-				log_error("Cannot modify removed %s event source (handle: %d) at index %d",
-				          event_get_source_type_name(type, false), handle, i);
-
-				return -1;
-			}
-
+	if (event_source != NULL) {
+		// readd removed event source
+		if (event_source->state == EVENT_SOURCE_STATE_REMOVED) {
 			memcpy(&backup, event_source, sizeof(backup));
 
-			// modify events bitmask
-			if ((event_source->events & events_to_remove) != events_to_remove) {
-				log_warn("Events to be removed (0x%04X) from %s event source (handle: %d) at index %d were not added before",
-				         events_to_remove, event_get_source_type_name(type, false), handle, i);
-			}
+			event_source->events = events;
+			event_source->state = EVENT_SOURCE_STATE_READDED;
 
-			event_source->events &= ~events_to_remove;
-
-			if ((event_source->events & events_to_add) != 0) {
-				log_warn("Events to be added (0x%04X) to %s event source (handle: %d) at index %d are already added",
-				         events_to_add, event_get_source_type_name(type, false), handle, i);
-			}
-
-			event_source->events |= events_to_add;
-
-			// unset functions for removed events
-			if ((events_to_remove & EVENT_READ) != 0) {
-				event_source->read = NULL;
-				event_source->read_opaque = NULL;
-			}
-
-			if ((events_to_remove & EVENT_WRITE) != 0) {
-				event_source->write = NULL;
-				event_source->write_opaque = NULL;
-			}
-
-			if ((events_to_remove & EVENT_PRIO) != 0) {
-				event_source->prio = NULL;
-				event_source->prio_opaque = NULL;
-			}
-
-			if ((events_to_remove & EVENT_ERROR) != 0) {
-				event_source->error = NULL;
-				event_source->error_opaque = NULL;
-			}
-
-			// set functions for added events
-			if ((events_to_add & EVENT_READ) != 0) {
+			if ((events & EVENT_READ) != 0) {
 				event_source->read = function;
 				event_source->read_opaque = opaque;
 			}
 
-			if ((events_to_add & EVENT_WRITE) != 0) {
+			if ((events & EVENT_WRITE) != 0) {
 				event_source->write = function;
 				event_source->write_opaque = opaque;
 			}
 
-			if ((events_to_add & EVENT_PRIO) != 0) {
+			if ((events & EVENT_PRIO) != 0) {
 				event_source->prio = function;
 				event_source->prio_opaque = opaque;
 			}
 
-			if ((events_to_add & EVENT_ERROR) != 0) {
+			if ((events & EVENT_ERROR) != 0) {
 				event_source->error = function;
 				event_source->error_opaque = opaque;
 			}
 
-			event_source->state = EVENT_SOURCE_STATE_MODIFIED;
-
-			if (event_source_modified_platform(event_source) < 0) {
+			if (event_source_added_platform(event_source) < 0) {
 				memcpy(event_source, &backup, sizeof(backup));
 
 				return -1;
 			}
 
-			log_debug("Modified (removed: 0x%04X, added: 0x%04X) %s event source (handle: %d) at index %d",
-			          events_to_remove, events_to_add,
-			          event_get_source_type_name(type, false), handle, i);
+			log_debug("Readded %s event source (handle: %d) at index %d",
+			          event_get_source_type_name(type, false), handle, index);
 
 			return 0;
 		}
+
+		log_error("%s event source (handle: %d) already added at index %d",
+		          event_get_source_type_name(type, true), handle, index);
+
+		return -1;
+	} else {
+		// add new event source
+		event_source = array_append(&_event_sources);
+
+		if (event_source == NULL) {
+			log_error("Could not append to event source array: %s (%d)",
+			          get_errno_name(errno), errno);
+
+			return -1;
+		}
+
+		event_source->handle = handle;
+		event_source->type = type;
+		event_source->events = events;
+		event_source->state = EVENT_SOURCE_STATE_ADDED;
+
+		if ((events & EVENT_READ) != 0) {
+			event_source->read = function;
+			event_source->read_opaque = opaque;
+		}
+
+		if ((events & EVENT_WRITE) != 0) {
+			event_source->write = function;
+			event_source->write_opaque = opaque;
+		}
+
+		if ((events & EVENT_PRIO) != 0) {
+			event_source->prio = function;
+			event_source->prio_opaque = opaque;
+		}
+
+		if ((events & EVENT_ERROR) != 0) {
+			event_source->error = function;
+			event_source->error_opaque = opaque;
+		}
+
+		if (event_source_added_platform(event_source) < 0) {
+			array_remove(&_event_sources, _event_sources.count - 1, NULL);
+
+			return -1;
+		}
+
+		log_debug("Added %s event source (handle: %d, events: %d) at index %d",
+		          event_get_source_type_name(type, false),
+		          handle, events, _event_sources.count - 1);
+
+		return 0;
+	}
+}
+
+// the events that an event source was added for can be modified
+int event_modify_source(IOHandle handle, EventSourceType type, uint32_t events_to_remove,
+                        uint32_t events_to_add, EventFunction function, void *opaque) {
+	int index;
+	EventSource *event_source;
+	EventSource backup;
+
+	event_source = event_find_source(0, _event_sources.count, handle, type, &index);
+
+	if (event_source == NULL) {
+		log_warn("Could not modify unknown %s event source (handle: %d)",
+		         event_get_source_type_name(type, false), handle);
+
+		return -1;
 	}
 
-	log_warn("Could not modify unknown %s event source (handle: %d)",
-	         event_get_source_type_name(type, false), handle);
+	if (event_source->state == EVENT_SOURCE_STATE_REMOVED) {
+		log_error("Cannot modify removed %s event source (handle: %d) at index %d",
+		          event_get_source_type_name(type, false), handle, index);
 
-	return -1;
+		return -1;
+	}
+
+	memcpy(&backup, event_source, sizeof(backup));
+
+	// modify events bitmask
+	if ((event_source->events & events_to_remove) != events_to_remove) {
+		log_warn("Events to be removed (0x%04X) from %s event source (handle: %d) at index %d were not added before",
+		         events_to_remove, event_get_source_type_name(type, false), handle, index);
+	}
+
+	event_source->events &= ~events_to_remove;
+
+	if ((event_source->events & events_to_add) != 0) {
+		log_warn("Events to be added (0x%04X) to %s event source (handle: %d) at index %d are already added",
+		         events_to_add, event_get_source_type_name(type, false), handle, index);
+	}
+
+	event_source->events |= events_to_add;
+
+	// unset functions for removed events
+	if ((events_to_remove & EVENT_READ) != 0) {
+		event_source->read = NULL;
+		event_source->read_opaque = NULL;
+	}
+
+	if ((events_to_remove & EVENT_WRITE) != 0) {
+		event_source->write = NULL;
+		event_source->write_opaque = NULL;
+	}
+
+	if ((events_to_remove & EVENT_PRIO) != 0) {
+		event_source->prio = NULL;
+		event_source->prio_opaque = NULL;
+	}
+
+	if ((events_to_remove & EVENT_ERROR) != 0) {
+		event_source->error = NULL;
+		event_source->error_opaque = NULL;
+	}
+
+	// set functions for added events
+	if ((events_to_add & EVENT_READ) != 0) {
+		event_source->read = function;
+		event_source->read_opaque = opaque;
+	}
+
+	if ((events_to_add & EVENT_WRITE) != 0) {
+		event_source->write = function;
+		event_source->write_opaque = opaque;
+	}
+
+	if ((events_to_add & EVENT_PRIO) != 0) {
+		event_source->prio = function;
+		event_source->prio_opaque = opaque;
+	}
+
+	if ((events_to_add & EVENT_ERROR) != 0) {
+		event_source->error = function;
+		event_source->error_opaque = opaque;
+	}
+
+	event_source->state = EVENT_SOURCE_STATE_MODIFIED;
+
+	if (event_source_modified_platform(event_source) < 0) {
+		memcpy(event_source, &backup, sizeof(backup));
+
+		return -1;
+	}
+
+	log_debug("Modified (removed: 0x%04X, added: 0x%04X) %s event source (handle: %d) at index %d",
+	          events_to_remove, events_to_add,
+	          event_get_source_type_name(type, false), handle, index);
+
+	return 0;
 }
 
 // only mark event sources as removed here, because the event loop might
 // be in the middle of iterating the event sources array when this function
 // is called
 void event_remove_source(IOHandle handle, EventSourceType type) {
-	int i;
+	int index;
 	EventSource *event_source;
 
 	// iterate backwards to remove the last added instance of an event source,
 	// otherwise an remove-add-remove sequence for the same event source between
 	// two calls to event_cleanup_sources doesn't work properly
-	for (i = _event_sources.count - 1; i >= 0; --i) {
-		event_source = array_get(&_event_sources, i);
+	event_source = event_find_source(_event_sources.count - 1, -1, handle, type, &index);
 
-		if (event_source->handle == handle &&
-		    event_source->type == type) {
-			if (event_source->state == EVENT_SOURCE_STATE_REMOVED) {
-				log_warn("%s event source (handle: %d, events: %d) already marked as removed at index %d",
-				         event_get_source_type_name(event_source->type, true),
-				         event_source->handle, event_source->events, i);
-			} else {
-				event_source->state = EVENT_SOURCE_STATE_REMOVED;
+	if (event_source == NULL) {
+		log_warn("Could not mark unknown %s event source (handle: %d) as removed",
+		         event_get_source_type_name(type, false), handle);
 
-				event_source_removed_platform(event_source);
-
-				log_debug("Marked %s event source (handle: %d, events: %d) as removed at index %d",
-				          event_get_source_type_name(event_source->type, false),
-				          event_source->handle, event_source->events, i);
-			}
-
-			return;
-		}
+		return;
 	}
 
-	log_warn("Could not mark unknown %s event source (handle: %d) as removed",
-	         event_get_source_type_name(type, false), handle);
+	if (event_source->state == EVENT_SOURCE_STATE_REMOVED) {
+		log_warn("%s event source (handle: %d, events: %d) already marked as removed at index %d",
+		         event_get_source_type_name(event_source->type, true),
+		         event_source->handle, event_source->events, index);
+	} else {
+		event_source->state = EVENT_SOURCE_STATE_REMOVED;
+
+		event_source_removed_platform(event_source);
+
+		log_debug("Marked %s event source (handle: %d, events: %d) as removed at index %d",
+		          event_get_source_type_name(event_source->type, false),
+		          event_source->handle, event_source->events, index);
+	}
 }
 
 // remove event sources that got marked as removed and mark (re-)added event
