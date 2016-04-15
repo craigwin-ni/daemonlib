9f3799df1cb0bfe500a02cbe480d46a35fb81e73
Matthias Bolte <matthias@tinkerforge.com>
2016-04-15 19:06:09 +0200

    uwp: GetVersion function doesn't exist anymore
    But UWP implies windows 10 anyway.

--- socket_winapi.c
+++ socket_winapi.c
@@ -198,6 +198,7 @@ int socket_set_dual_stack(Socket *socket, bool dual_stack) {
 	DWORD on = dual_stack ? 0 : 1;
 	int rc;
 
+#ifndef DAEMONLIB_UWP_BUILD // UWP implies Windows 10
 	if ((DWORD)(LOBYTE(LOWORD(GetVersion()))) < 6) {
 		// the IPV6_V6ONLY option is only supported on Vista or later. on
 		// Windows XP dual-stack mode is not supported at all. so fail with
@@ -212,6 +213,7 @@ int socket_set_dual_stack(Socket *socket, bool dual_stack) {
 
 		return 0;
 	}
+#endif
 
 	rc = setsockopt(socket->base.handle, IPPROTO_IPV6, IPV6_V6ONLY,
 	                (const char *)&on, sizeof(on));
